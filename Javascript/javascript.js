d1db156b21ecbdd61aef050058f44d56938c1b71
mattiaaroo <mattiapmro@gmail.com>
2024-07-02 13:57:59 +0200

    commit

--- Javascript/javascript.js
+++ Javascript/javascript.js
@@ -1,4 +1,4 @@
-const prompt =  require('promt-sync')();
+const prompt =  require('prompt-sync')();
 const str = prompt('Enter a string: ');
 console.log("Hai Inserito " + str);
 
@@ -6,7 +6,15 @@ var sPrimoNum = prompt("Inserisci il primo numero ");
 var iPrimoNum = parseInt(sPrimoNum)
 
 var sSecondoNum = prompt("Inserisci secondo numero ");
+var iSecondoNum = parseInt(sSecondoNum)
 
-var sSomma = sPrimoNum + sSecondoNum
+var sSomma = iPrimoNum + iSecondoNum
 
-console.log("La")
+console.log("La somma vale " + sSomma );
+
+
+var sStringa = prompt("Inserisci la tua stringa: ")
+var iNum = prompt("Inserisci il numero da cercare: ")
+
+/* return none or number in the string */
+
