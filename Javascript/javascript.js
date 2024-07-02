eb0a9e05e5230ba37f0a9789073b149059e92e39
mattiaaroo <mattiapmro@gmail.com>
2024-07-02 12:18:14 +0200

    jj

---
+++ Javascript/javascript.js
@@ -0,0 +1,12 @@
+const prompt =  require('promt-sync')();
+const str = prompt('Enter a string: ');
+console.log("Hai Inserito " + str);
+
+var sPrimoNum = prompt("Inserisci il primo numero ");
+var iPrimoNum = parseInt(sPrimoNum)
+
+var sSecondoNum = prompt("Inserisci secondo numero ");
+
+var sSomma = sPrimoNum + sSecondoNum
+
+console.log("La")
